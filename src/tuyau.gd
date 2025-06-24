166a0240be63370116acf096a608a885e486d478
Clotaire MARDON <Clotaire.MARDON@cea.fr>
2025-06-24 11:23:10 +0200

    tuyau update

--- src/tuyau.gd
+++ src/tuyau.gd
@@ -5,13 +5,14 @@ extends Node2D
 @onready var tuyau_bas: TileMapLayer = %tuyau_bas
 
 @export var vitesse = 192
-@export var max_offset = 96
 @export var distance_entre_tuyaux: int = 40:
 	set(value):
 		if tuyau_bas:
 			distance_entre_tuyaux = value
 			tuyau_haut.position.y = -distance_entre_tuyaux/2
 			tuyau_bas.position.y = distance_entre_tuyaux/2
+
+var max_offset = 96
 
 func _ready():
 	var offset = randf_range(-max_offset, max_offset)
