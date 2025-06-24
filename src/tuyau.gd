51fe515c38006a94cf5ebf74eb3f0185ac7375ec
Clotaire MARDON <Clotaire.MARDON@cea.fr>
2025-06-24 14:05:55 +0200

    tuyau update

--- src/tuyau.gd
+++ src/tuyau.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var tuyau_bas: TileMapLayer = %tuyau_bas
 
 @export var vitesse = 192
-@export var distance_entre_tuyaux: int = 40:
+@export var distance_entre_tuyaux: int = 80:
 	set(value):
 		if tuyau_bas:
 			distance_entre_tuyaux = value
@@ -21,3 +21,7 @@ func _ready():
 func _process(delta):
 	if !Engine.is_editor_hint():
 		position.x -= vitesse * delta
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://src/niveau1.tscn")
